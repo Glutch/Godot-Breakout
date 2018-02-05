5b2d3f5f96442fd33969a18c1c88644e567e3f37
Glutch <oliverwiljohansson@gmail.com>
2018-02-05 16:11:10 +0100

    first commit

---
+++ Scripts/Paddle.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	pass
+
+func _process(delta):
+	var y = get_position().y
+	var x = get_viewport().get_mouse_position().x
+	set_position(Vector2(x, y))
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	pass
